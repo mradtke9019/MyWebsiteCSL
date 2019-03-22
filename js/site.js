11e87176c7a9cd1d00bd29e4e61d05738fcd15aa
Matt Radtke <mradtke9019@yahoo.com>
2019-03-21 21:00:33 -0500

    Offset is now fixed for scrolling

--- js/site.js
+++ js/site.js
@@ -6,7 +6,7 @@ $('a[href^="#"]').on('click', function (event) {
     if (target.length) {
         event.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: target.offset().top - target.height()/3
+            scrollTop: target.offset().top - 150
         }, 1000);
     }
 });
